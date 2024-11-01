6b13e2a57ca02e1a1b30e468eb9b7a298a96ed59
Typej2003 <typej2003@gmail.com>
2024-11-01 15:26:11 -0400

    update

--- storage/framework/views/b9ee3f2191dd30ed8adc30d8705088316a3c4fcf.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-	<!-- To the right -->
-	<div class="float-right d-none d-sm-inline">
-	  <!-- Anything you want -->
-	</div>
-	<!-- Default to the left -->
-	<strong>Copyright &copy; <?php echo e(date('Y')); ?> <a href="https://youtube.com/clovon" target="_blank">Clovon</a>.</strong> All rights reserved.
-</footer>
-<?php /**PATH C:\Users\Personal\Documents\Proyectos\pasarela\resources\views/layouts/partials/footer.blade.php ENDPATH**/ ?>
